ce3426cbbc572d57ddaf23a04364bb3b4766149d
Felipe Lobo <felipeblobo@gmail.com>
2021-04-18 17:52:57 -0300

    adequando par amultiplos dispositivos

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const buttonOnClick = document.querySelector('button');
 const formMovement = document.querySelector('form');
+
+
 buttonOnClick.addEventListener('click', (event) => {
   event.preventDefault();
-  formMovement.classList.add('moveDown');
+
+  const fields = [...document.querySelectorAll("input")]
+  fields.forEach((field) => {
+    if(field.value === '') {
+      formMovement.classList.add('validate-error')
+    }
+  });
+
+  const formError = document.querySelector('.validate-error')
+  if (formError) {
+    formError.addEventListener('animationend', (event) => {
+      if(event.animationName === 'nono') {
+        formError.classList.remove('validate-error');
+      }
+    })
+  } else {
+    formMovement.classList.add('moveDown');
+  }
 });
+
 
 formMovement.addEventListener('animationstart', (event) => {
   if (event.animationName === 'move') {
